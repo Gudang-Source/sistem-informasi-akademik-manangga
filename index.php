33efb0a3b06ec827a5f2bbc21d0e6ef11ef0b264
vanmovic <vanmovic@gmail.com>
2016-09-19 22:47:23 +0700

    index

--- index.php
+++ index.php
@@ -14,24 +14,24 @@ $ui_register_bg   = 'secondary';
 $ui_register_page = 'index';
 
 // LOAD HEADER
-loadAssetsHead('E-Learning SMK N 4 Klaten');
+loadAssetsHead('Sistem Informasi Akademik SDN II Manangga');
 
 ?>
 
 <body>
   <div class="uk-vertical-align uk-text-center uk-height-1-1">
     <div class="uk-vertical-align-middle">
-      <img class="uk-margin-bottom uk-animation-fade uk-animation-10,5" width="70%" height="70%" src="assets/images/banner.png" alt="E-Learning SMK Negeri 4 Klaten" title="E-Learning SMK Negeri 4 Klaten">
-		<br><br><br>
-      <a href="./login" target="_self"><img class="uk-margin-bottom uk-animation-fade uk-animation-10,5" width="150px" src="assets/images/button_masuk.png" alt="E-Learning SMK Negeri 4 Klaten" title="E-Learning SMK Negeri 4 Klaten"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <a href="./tentang" target="_self"><img class="uk-margin-bottom uk-animation-fade uk-animation-10,5" width="150px" src="assets/images/button_tentang.png" alt="E-Learning SMK Negeri 4 Klaten" title="E-Learning SMK Negeri 4 Klaten"></a>
-	</div>
+      <img class="uk-margin-bottom uk-animation-fade uk-animation-10,5" width="70%" height="70%" src="assets/images/banner.png" alt="Sistem Informasi Akademik SDN II Manangga" title="Sistem Informasi Akademik SDN II Manangga">
+    <br><br><br>
+      <a href="./login" target="_self"><img class="uk-margin-bottom uk-animation-fade uk-animation-10,5" width="150px" src="assets/images/button_masuk.png" alt="Sistem Informasi Akademik SDN II Manangga" title="Sistem Informasi Akademik SDN II Manangga"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="./tentang" target="_self"><img class="uk-margin-bottom uk-animation-fade uk-animation-10,5" width="150px" src="assets/images/button_tentang.png" alt="Sistem Informasi Akademik SDN II Manangga" title="Sistem Informasi Akademik SDN II Manangga"></a>
+  </div>
   </div>
 
 
 <?php
 // LOAD FOOTER
-loadAssetsFoot('');
+loadAssetsFoot();
 
 ob_end_flush();
 ?>
