5bb7e2ad4149b656dc999ab91902d610c050a1ef
fajar <fajarnur24@gmail.com>
2016-09-20 00:52:57 +0700

    hapus berita

--- berita.hapus.php
+++ berita.hapus.php
@@ -1,5 +1,11 @@
 <?php
 require ( __DIR__ . '/init.php');
-mysql_query("DELETE from woroworo WHERE id_woro='$_GET[id]'");
-header('location: ./dashboard');
+mysql_query("DELETE from berita WHERE id_berita='$_GET[id]'");
+unlink("../assets/user/images/slider/$_GET[file]");
+
+echo '<script language="javascript">';
+echo 'alert("Data Berhasil Dihapus")';
+echo '</script>';
+echo "<script> document.location.href='dashboard.php';</script>";
+
 ?>
