696dd8279901402c21a4f617e54e1b0f24996506
vanmovic <vanmovic@gmail.com>
2016-09-19 23:02:56 +0700

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,14 +2,13 @@
 					<div class="nengah">
 						<div class="app-row">
 							<div class="col-med-3">
-								
+								<a href="" target="_blank"><img src="assets/images/bawah.png"></a>
 							</div>
 							<div class="col-med-3">
 								<div style="float:left" class="alamat">
-									Jl. Mataram No.5 Belang Wetan<br>
-									 Klaten Utara<br>
-									 Klaten<br>
-									 Telp. 0272-324084<br>
+									 Kp. Rancageneng <br>
+									 Sukajaya, Bungursari, Kota Tasikmalaya <br>
+									 Jawa Barat <br>
 								</div>
 							</div>
 							<div class="col-med-3">
@@ -19,21 +18,21 @@
 									<a href="#">Privasi &amp; Kebijakan</a>
 								</div>
 							</div>
-							<div class="col-med-3">
+							<div class="col-med-3">	
 								<div class="social-media">
 									<a style="margin-left:6px; display:inline;" title="Google Plus" href="#" target="_blank"><img src="assets/images/icons/gplus.png" height="30" width="30"></a>
-									<a style="margin-left:7px; display:inline;" title="Facebook" href="https://www.facebook.com/" target="_blank"><img src="assets/images/icons/facebook.png" height="30" width="30"></a>
-									<a style="margin-left:6px; display:inline;" title="Twitter" href="https://twitter.com/" target="_blank"><img src="assets/images/icons/twitter.png" height="30" width="30"></a>
+									<a style="margin-left:7px; display:inline;" title="Facebook" href="https://www.facebook.com/pages/SMA-N-2-PLAYEN/115735181774436" target="_blank"><img src="assets/images/icons/facebook.png" height="30" width="30"></a>
+									<a style="margin-left:6px; display:inline;" title="Twitter" href="https://twitter.com/_duplayKeras" target="_blank"><img src="assets/images/icons/twitter.png" height="30" width="30"></a>
 									<a style="margin-left:6px; display:inline;" title="Youtube" href="#" target="_blank"><img src="assets/images/icons/youtube.png" height="30" width="30"></a>
 									<a style="margin-left:6px; display:inline;" title="Instagram" href="#" target="_blank"><img src="assets/images/icons/instagram.png" height="30" width="30"></a>
 									<a style="margin-left:6px; display:inline;" title="Foursquare" href="#" target="_blank"><img src="assets/images/icons/foursquare.png" height="30" width="30"></a>
 								</div>
 							</div>
-						</div>
+						</div>	
 						<div class="clear20"></div>
-					
+
 							</div>
-						</div>
+						</div>	
 						<div style="clear:both;"></div>
 					</div>
 			</div>
