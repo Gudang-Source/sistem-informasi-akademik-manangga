47892e0b347562b4564679a0e563078664f7383f
fajar <fajarnur24@gmail.com>
2016-09-23 18:29:40 +0700

    guru tambah

--- guru.tambah.php
+++ guru.tambah.php
@@ -15,7 +15,7 @@ loadAssetsHead('Tambah Master Data guru');
 if (isset ($_POST["guru_simpan"])) { 
 
     // baca variabel
-    
+
   $nip  = $_POST['nip'];
   $password  = $_POST['password'];
   $nm_guru  = $_POST['nm_guru'];
@@ -29,38 +29,38 @@ if (isset ($_POST["guru_simpan"])) {
   $id_user =2;
 
     // validation form kosong
-   $pesanError= array();
+  $pesanError= array();
   if (trim($nip)=="") {
     $pesanError[]="Data <b>NIP</b> masih kosong.";
   }
-   if (trim($password)=="") {
+  if (trim($password)=="") {
     $pesanError[]="Data <b>Password</b> masih kosong.";
   }
   if (trim($nm_guru)=="") {
     $pesanError[]="Data <b>Nama Gru</b> masih kosong.";
   }
-   if (trim($tmpt_lahir)=="") {
+  if (trim($tmpt_lahir)=="") {
     $pesanError[]="Data <b>Tempat Lahir</b> masih kosong.";
   }
-    if (trim($date_tgl_lahir)=="") {
+  if (trim($date_tgl_lahir)=="") {
     $pesanError[]="Data <b>Tanggal Lahir</b> masih kosong.";
   }
-    if (trim($jns_kelamin)=="") {
+  if (trim($jns_kelamin)=="") {
     $pesanError[]="Data <b>Jenis Kelamin</b> masih kosong.";
   }
-    if (trim($agama)=="") {
+  if (trim($agama)=="") {
     $pesanError[]="Data <b>Agama</b> masih kosong.";
   }
-   if (trim($almt_sekarang)=="") {
+  if (trim($almt_sekarang)=="") {
     $pesanError[]="Data <b>Alamat Sekarang</b> masih kosong.";
   }
-      if (trim($no_hp)=="") {
+  if (trim($no_hp)=="") {
     $pesanError[]="Data <b>Nomor HP</b> masih kosong.";
   }
-      if (trim($email)=="") {
+  if (trim($email)=="") {
     $pesanError[]="Data <b>Email</b> masih kosong.";
   }
-   
+
     // validasi nip pada database
   $cekSql ="SELECT * FROM guru WHERE nip='$nip'";
   $cekQry = mysql_query($cekSql) or die("Error Query:".mysql_error());
@@ -69,40 +69,40 @@ if (isset ($_POST["guru_simpan"])) {
   }
 
     // jika ada error dari validasi form
-     if (count($pesanError)>=1) {
+  if (count($pesanError)>=1) {
     echo "<div class='mssgBox'>";
     echo "<img src ='../images/attention.png'><br><hr>";
     $noPesan= 0;
     foreach ($pesanError as $indeks => $pesan_tampil) {
       $noPesan++;
       echo "&nbsp;&nbsp; $noPesan. $pesan_tampil<br>";
-     }
-    echo "</div><br />";
     }
+    echo "</div><br />";
+  }
 
-    else{
+  else{
 
     // simpan ke database
-  $querytambahguru = mysql_query("INSERT INTO guru (nip, id_user, password, nm_guru, tmpt_lahir, date_tgl_lahir, jns_kelamin, agama, almt_sekarang, no_hp, email) 
-    VALUES ( '$nip', '$id_user', '$password', '$nm_guru', '$tmpt_lahir', '$date_tgl_lahir', '$jns_kelamin', '$agama', '$almt_sekarang', '$no_hp', '$email' )") or die(mysql_error());
+    $querytambahguru = mysql_query("INSERT INTO guru (nip, id_user, password, nm_guru, tmpt_lahir, date_tgl_lahir, jns_kelamin, agama, almt_sekarang, no_hp, email) 
+      VALUES ( '$nip', '$id_user', '$password', '$nm_guru', '$tmpt_lahir', '$date_tgl_lahir', '$jns_kelamin', '$agama', '$almt_sekarang', '$no_hp', '$email' )") or die(mysql_error());
 
-  if ($querytambahguru){
-    header('location: ./guru');
+    if ($querytambahguru){
+      header('location: ./guru');
+    }
   }
- }
 }
 
     // simpan pada form, dan jika form belum terisi
-  $datanip  = isset($_POST['nip']) ? $_POST['nip'] : '';
-  $datapassword  = isset($_POST['password']) ? $_POST['password'] : '';
-  $datanamaguru  = isset($_POST['nm_guru']) ? $_POST['nm_guru'] : '';
-  $datatempatlahir  = isset($_POST['tmpt_lahir']) ? $_POST['tmpt_lahir'] : '';
-  $datatanggallahir  = isset($_POST['date_tgl_lahir']) ? $_POST['date_tgl_lahir'] : '';
-  $datajeniskelamin  = isset($_POST['jns_kelamin']) ? $_POST['jns_kelamin'] : '';
-  $dataagama  = isset($_POST['agama']) ? $_POST['agama'] : '';
-  $dataalamat  = isset($_POST['almt_sekarang']) ? $_POST['almt_sekarang'] : '';
-  $datanohp  = isset($_POST['no_hp']) ? $_POST['no_hp'] : '';
-  $dataemail  = isset($_POST['email']) ? $_POST['email'] : '';
+$datanip  = isset($_POST['nip']) ? $_POST['nip'] : '';
+$datapassword  = isset($_POST['password']) ? $_POST['password'] : '';
+$datanamaguru  = isset($_POST['nm_guru']) ? $_POST['nm_guru'] : '';
+$datatempatlahir  = isset($_POST['tmpt_lahir']) ? $_POST['tmpt_lahir'] : '';
+$datatanggallahir  = isset($_POST['date_tgl_lahir']) ? $_POST['date_tgl_lahir'] : '';
+$datajeniskelamin  = isset($_POST['jns_kelamin']) ? $_POST['jns_kelamin'] : '';
+$dataagama  = isset($_POST['agama']) ? $_POST['agama'] : '';
+$dataalamat  = isset($_POST['almt_sekarang']) ? $_POST['almt_sekarang'] : '';
+$datanohp  = isset($_POST['no_hp']) ? $_POST['no_hp'] : '';
+$dataemail  = isset($_POST['email']) ? $_POST['email'] : '';
 
 ?>
 
@@ -131,109 +131,171 @@ if (isset ($_POST["guru_simpan"])) {
           <div class="uk-grid" data-uk-grid-margin>
             <div class="uk-width-medium-1-1">
              <form id="formguru" method="POST" class="form-horizontal form-label-left" enctype="multipart/form-data">
-        
-        <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nip">NIP<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="nip" name="nip" value="<?php echo $datanip; ?>" required="required" class="form-control col-md-7 col-xs-12">
-            <div class="reg-info">Contoh: 126500182411. Jumlah minimal 18 angka. Wajib Diisi (Digunakan sebagai username untuk login)</div>
-          </div>
-        </div>
 
-        <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nm_guru">Nama Guru<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="nm_guru" name="nm_guru" value="<?php echo $datanamaguru; ?>" required="required" class="form-control col-md-7 col-xs-12">
-          </div>
-        </div>
+              <div class="item form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="foto">Image <span class="required">*</span>
+                </label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <div class="col-lg-8">
+                    <div class="fileupload fileupload-new" data-provides="fileupload">
+                      <div class="fileupload-new thumbnail" style="width: 200px; height: 150px;"><img src="./assets/fileupload/images/nopict.jpg" alt="" /></div>
+                      <div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
+                      <div>
+                        <span class="btn btn-file btn-primary btn-xs"><span class="fileupload-new">Select image</span><span class="fileupload-exists">Change</span><input type="file" accept="image/*" name="file" id="file" placeholder="file" /></span>
+                        <a href="#" class="btn btn-danger btn-xs fileupload-exists" data-dismiss="fileupload">Remove</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="item form-group">
+               <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nip">NIP<span class="required">*</span>
+               </label>
+               <div class="col-md-6 col-sm-6 col-xs-12">
+                <input type="text" id="nip" name="nip" value="<?php echo $datanip; ?>" required="required" class="form-control col-md-7 col-xs-12">
+                <div class="reg-info">Contoh: 126500182411. Jumlah minimal 18 angka. Wajib Diisi (Digunakan sebagai username untuk login)</div>
+              </div>
+            </div>
 
-   <div class="item form-group">
+            <div class="item form-group">
+             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nm_guru">Nama Guru<span class="required">*</span>
+             </label>
+             <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="nm_guru" name="nm_guru" value="<?php echo $datanamaguru; ?>" required="required" class="form-control col-md-7 col-xs-12">
+              <div class="reg-info">Contoh: Fajar Nurrohamt. Jumlah minimal 1 huruf. Wajib Diisi (Tuliskan Nama Saja, tidak dengan title)</div>
+            </div>
+          </div>
+
+          <div class="item form-group">
            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password">Password<span class="required">*</span>
            </label>
            <div class="col-md-6 col-sm-6 col-xs-12">
             <input type="text" id="password" name="password" value="<?php echo $datapassword; ?>" required="required" class="form-control col-md-7 col-xs-12">
+            <div class="reg-info">Contoh: 126500182411. Jumlah minimal 6 karakter. Wajib Diisi (Digunakan untuk login)</div>
           </div>
         </div>
-
-       <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="date_tgl_lahir">Tanggal Lahir<span class="required">*</span>
+          <div class="item form-group">
+           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="password1">Konfirmasi Password<span class="required">*</span>
            </label>
            <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="date_tgl_lahir" name="date_tgl_lahir" value="<?php echo $datatanggallahir; ?>" required="required" class="form-control col-md-7 col-xs-12" data-uk-datepicker="{format:'YYYY/DD/MM'}" >
-            <div class="reg-info">Format: <code>TTTT/HH/BB</code></div>
-            <div class="reg-info">Contoh: 1995/31/12</div>
-          </div>
-       </div>
-
-      <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="tmpt_lahir">Tempat Lahir<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="tmpt_lahir" name="tmpt_lahir" value="<?php echo $datatempatlahir; ?>" required="required" class="form-control col-md-7 col-xs-12">
-          </div>
-        </div>
-
-      <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="jns_kelamin">Jenis Kelamin<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <select name="jns_kelamin" id="jns_kelamin" value="<?php echo $datajeniskelamin; ?>" class="form-control col-md-7 col-xs-12">
-              <option value="">--- Pilih Jenis Kelamin --</option>
-              <option value="Laki-Laki">Laki-Laki</option>
-              <option value="Perempuan">Perempuan</option>
-            </select>
-          </div>
-        </div>
-
-      <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="agama">Agama<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <select name="agama" id="agama" value="<?php echo $dataagama; ?>" class="form-control col-md-7 col-xs-12">
-              <option value="">--- Pilih agama yang dianut --</option>
-              <option value="Islam">Islam</option>
-              <option value="Kristen Protestan">Kristen Protestan</option>
-              <option value="Kristen Katholik">Kristen Katholik</option>
-              <option value="Hindu">Hindu</option>
-              <option value="Buddha">Buddha</option>
-              <option value="Konghuchu">Konghuchu</option>
-            </select>
+            <input type="text" id="password1" name="password1" value="<?php echo $datapassword; ?>" required="required" class="form-control col-md-7 col-xs-12">
+             <div class="reg-info">Contoh: 126500182411. Jumlah minimal 6 karakter. Harus Sama dengan Password. Wajib Diisi (Digunakan untuk login)</div>
           </div>
         </div>
 
         <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="almt_sekarang">Alamat Rumah<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="almt_sekarang" name="almt_sekarang" value="<?php echo $dataalamat; ?>" required="required" class="form-control col-md-7 col-xs-12">
-          </div>
-       </div>
+         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="date_tgl_lahir">Tanggal Lahir<span class="required">*</span>
+         </label>
+         <div class="col-md-6 col-sm-6 col-xs-12">
+          <input type="text" id="date_tgl_lahir" name="date_tgl_lahir" value="<?php echo $datatanggallahir; ?>" required="required" class="form-control col-md-7 col-xs-12" data-uk-datepicker="{format:'YYYY/DD/MM'}" >
+          <div class="reg-info">Format: <code>TTTT/HH/BB</code></div>
+          <div class="reg-info">Contoh: 1995/31/12</div>
+        </div>
+      </div>
 
       <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="no_hp">No. HP<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="no_hp" name="no_hp" value="<?php echo $datanohp; ?>" required="required" class="form-control col-md-7 col-xs-12">
-          </div>
-       </div>
+       <label class="control-label col-md-3 col-sm-3 col-xs-12" for="tmpt_lahir">Tempat Lahir<span class="required">*</span>
+       </label>
+       <div class="col-md-6 col-sm-6 col-xs-12">
+        <input type="text" id="tmpt_lahir" name="tmpt_lahir" value="<?php echo $datatempatlahir; ?>" required="required" class="form-control col-md-7 col-xs-12">
+         <div class="reg-info">contoh: Gunungkidul, Tempat Lahir berupa nama kabupaten</div>
+      </div>
+    </div>
 
-         <div class="item form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">Email<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="email" name="email" value="<?php echo $dataemail; ?>" required="required" class="form-control col-md-7 col-xs-12">
-          </div>
-       </div>
+    <div class="item form-group">
+     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="jns_kelamin">Jenis Kelamin<span class="required">*</span>
+     </label>
+     <div class="col-md-6 col-sm-6 col-xs-12">
+      <select name="jns_kelamin" id="jns_kelamin" value="<?php echo $datajeniskelamin; ?>" class="form-control col-md-7 col-xs-12">
+        <option value="">--- Pilih Jenis Kelamin --</option>
+        <option value="Laki-Laki">Laki-Laki</option>
+        <option value="Perempuan">Perempuan</option>
+      </select>
+       <div class="reg-info">Jenis Kelamin Hanya Laki-Laki dan Perempuan , Pilih Salah Satu!</div>
+    </div>
+  </div>
 
-        <div class="uk-form-row">
-                   <div class="uk-alert">Pastikan semua isian sudah terisi dengan benar !</div>
-        </div>
-        <div style="text-align:center" class="form-actions no-margin-bottom">
-         <button type="submit" id="guru_simpan" name="guru_simpan" class="btn btn-success">Submit</button>
-       </div>
-     </form>    
+  <div class="item form-group">
+   <label class="control-label col-md-3 col-sm-3 col-xs-12" for="agama">Agama<span class="required">*</span>
+   </label>
+   <div class="col-md-6 col-sm-6 col-xs-12">
+    <select name="agama" id="agama" value="<?php echo $dataagama; ?>" class="form-control col-md-7 col-xs-12">
+      <option value="">--- Pilih agama yang dianut --</option>
+      <option value="Islam">Islam</option>
+      <option value="Kristen Protestan">Kristen Protestan</option>
+      <option value="Kristen Katholik">Kristen Katholik</option>
+      <option value="Hindu">Hindu</option>
+      <option value="Buddha">Buddha</option>
+      <option value="Konghuchu">Konghuchu</option>
+    </select>
+     <div class="reg-info">Pilihan Agama yang ada hanya terdapat di pilihan, apabila tidak ada dalam menu pilihan agama ,harap menghubungi di bagian kesiswaan</div>
+  </div>
+</div>
+<div class="item form-group">
+     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="jns_kelamin">Status Guru<span class="required">*</span>
+     </label>
+     <div class="col-md-6 col-sm-6 col-xs-12">
+      <select name="status_guru" id="status_guru" value="<?php echo $datastatusguru; ?>" class="form-control col-md-7 col-xs-12">
+        <option value="">--- Pilih Status Guru --</option>
+        <option value="wiyatabhakti">Wiyata Bhakti</option>
+        <option value="Pns">Pegawai Negri Sipil</option>
+      </select>
+       <div class="reg-info">Status Guru Hanya Wiyata Bhakti dan Pegawai Negri Sipil(PNS) , Pilih Salah Satu!</div>
+    </div>
+  </div>
+
+  <div class="item form-group">
+ <label class="control-label col-md-3 col-sm-3 col-xs-12" for="gelar_depan">Gelar Depan Non Akademik<span class="required">*</span>
+ </label>
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <input type="text" id="gelar_depan" name="gelar_depan" value="<?php echo $dataalamat; ?>" required="required" class="form-control col-md-7 col-xs-12">
+</div>
+</div>
+<div class="item form-group">
+ <label class="control-label col-md-3 col-sm-3 col-xs-12" for="almt_sekarang">Alamat Rumah<span class="required">*</span>
+ </label>
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <input type="text" id="almt_sekarang" name="almt_sekarang" value="<?php echo $dataalamat; ?>" required="required" class="form-control col-md-7 col-xs-12">
+</div>
+</div>
+<div class="item form-group">
+ <label class="control-label col-md-3 col-sm-3 col-xs-12" for="almt_sekarang">Alamat Rumah<span class="required">*</span>
+ </label>
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <input type="text" id="almt_sekarang" name="almt_sekarang" value="<?php echo $dataalamat; ?>" required="required" class="form-control col-md-7 col-xs-12">
+</div>
+</div>
+<div class="item form-group">
+ <label class="control-label col-md-3 col-sm-3 col-xs-12" for="almt_sekarang">Alamat Rumah<span class="required">*</span>
+ </label>
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <input type="text" id="almt_sekarang" name="almt_sekarang" value="<?php echo $dataalamat; ?>" required="required" class="form-control col-md-7 col-xs-12">
+</div>
+</div>
+
+<div class="item form-group">
+ <label class="control-label col-md-3 col-sm-3 col-xs-12" for="no_hp">No. HP<span class="required">*</span>
+ </label>
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <input type="text" id="no_hp" name="no_hp" value="<?php echo $datanohp; ?>" required="required" class="form-control col-md-7 col-xs-12">
+</div>
+</div>
+
+<div class="item form-group">
+ <label class="control-label col-md-3 col-sm-3 col-xs-12" for="email">Email<span class="required">*</span>
+ </label>
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <input type="text" id="email" name="email" value="<?php echo $dataemail; ?>" required="required" class="form-control col-md-7 col-xs-12">
+</div>
+</div>
+
+<div class="uk-form-row">
+ <div class="uk-alert">Pastikan semua isian sudah terisi dengan benar !</div>
+</div>
+<div style="text-align:center" class="form-actions no-margin-bottom">
+ <button type="submit" id="guru_simpan" name="guru_simpan" class="btn btn-success">Submit</button>
+</div>
+</form>    
 </div>
 </div>
 </div>
@@ -265,13 +327,13 @@ if (isset ($_POST["guru_simpan"])) {
       max: 18,
       message: 'NIP minimal 18 angka.'
     },
-     remote: {
+    remote: {
       type: 'POST',
       url: 'remote/remote_guru.php',
       message: 'NIP Guru Telah Tersedia'
     },
-   }
- }, 
+  }
+}, 
 
 }
 });
