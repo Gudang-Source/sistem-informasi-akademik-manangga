0e159bbb2c6370190529b0fc2c87c63cd77b0d72
fajar nurrohmat <fajarnur24@gmail.com>
2016-12-03 18:29:57 +0700

    tahun ajaran

--- tahun-ajaran.tambah.php
+++ tahun-ajaran.tambah.php
@@ -44,11 +44,22 @@ if (isset ($_POST["tahun_simpan"])) {
     echo "</div><br />";
     }
 
-    else{
+   else{
 
+$cektahun = mysql_query("SELECT * FROM tahun_ajaran WHERE status='1'");
+          mysql_num_rows($cektahun);
+          $tahun_ajaransession = mysql_fetch_array($cektahun);          
+
+          // tahun ajaran session
+          $_SESSION['id_tahun'] = $tahun_ajaransession['id_tahun'];
+          $_SESSION['thn_ajaran'] = $tahun_ajaransession['thn_ajaran'];
+          $_SESSION['semester'] = $tahun_ajaransession['semester'];
+          $_SESSION['status'] = $tahun_ajaransession['status'];
     // simpan ke database
-  $querytambahtahun = mysql_query("INSERT INTO tahun_ajaran (thn_ajaran, semester) 
-    VALUES ('$thn_ajaran' , '$semester' )") or die(mysql_error());
+  $querytambahtahun = mysql_query("INSERT INTO tahun_ajaran (thn_ajaran, semester,status) 
+    VALUES ('$thn_ajaran' , '$semester','0' )") or die(mysql_error());
+  $update=mysql_query("UPDATE tahun_ajaran SET  status='0' WHERE status='1'");
+  $update=mysql_query("UPDATE tahun_ajaran SET  status='1' WHERE id_tahun='$tahun_ajaransession[id_tahun]'");
 
   if ($querytambahtahun){
         $cektahun = mysql_query("SELECT * FROM tahun_ajaran WHERE status='1'");
@@ -64,6 +75,7 @@ if (isset ($_POST["tahun_simpan"])) {
   }
  }
 }
+
 
     // simpan pada form, dan jika form belum terisi
   $datanamatahunajaran = isset($_POST['thn_ajaran']) ? $_POST['thn_ajaran'] : '';
