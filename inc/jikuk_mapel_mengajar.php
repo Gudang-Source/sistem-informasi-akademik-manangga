0010727b0fcecf37f6b8acea8ec19a0c819561f7
fajar nurrohmat <fajarnur24@gmail.com>
2016-12-04 00:51:39 +0700

    jikuk jadwal

--- inc/jikuk_mapel_mengajar.php
+++ inc/jikuk_mapel_mengajar.php
@@ -2,7 +2,10 @@
 require ('../config.php');
 $id_guru = $_GET['id_guru'];
 //$mapel = mysql_query("SELECT * from mapel ");
-$mapel = mysql_query("SELECT mapel.kd_mapel,mapel.nm_mapel FROM mengajar, mapel WHERE mengajar.kd_mapel=mapel.kd_mapel and mengajar.id_guru='$id_guru' order by mapel.nm_mapel");
+$mapel = mysql_query("SELECT mapel.kd_mapel,mapel.nm_mapel FROM mapel left join mengajar on mengajar.kd_mapel=mapel.kd_mapel 
+					where 
+					mengajar.id_guru='$id_guru' 
+					order by mapel.nm_mapel");
 echo "<option>-- Pilih Mata Pelajaran --</option>";
 while($k = mysql_fetch_array($mapel)){
     echo "<option value=\"".$k['kd_mapel']."\">".$k['nm_mapel']."</option>\n";
