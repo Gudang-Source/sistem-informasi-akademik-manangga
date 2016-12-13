9b20dbfd7fc04a71e464cb74174f955110dda352
fajar nurrohmat <fajarnur24@gmail.com>
2016-12-13 22:22:06 +0700

    nilai

--- action.nilai.php
+++ action.nilai.php
@@ -91,7 +91,9 @@ $uas= !empty($uas) ? "'$uas'" : "NULL";
 	$kd_mapel=$_POST['kd_mapel'];
 	$id_kelas_siswa_edit=$_POST['id_kelas_siswa_edit'];
 	$id_tahun_edit=$_POST['id_tahun_edit'];
-	
+
+	$uhtotal= (($uh1+$uh2+$uh3+$uh4+$uh5+$uh6+$uh7)/7)*20;
+	$tugastotal= ((t1+t2+t3+t4+t5+t6+t7)/7)*;
 	
 	$updatenilai=mysql_query("UPDATE nilai SET uh1=$uh1, 
 											   uh2=$uh2, 
